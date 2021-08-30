39ad525dcc940b88553c3944a2fc92cf3fb187e6
Calli-T <joy14479@gmail.com>
2021-08-30 10:19:43 +0900

    https://cocoon1787.tistory.com/317 를 보고 11066번을 어떻게 풀지를 배웠다

--- DP/동적 계획법2/11066/11066/소스.cpp
+++ DP/동적 계획법2/11066/11066/소스.cpp
@@ -1,59 +1,73 @@
 #include <iostream>
 #include <vector>
 
+#define INF 1000000000
+
 using namespace std;
 
-#define INF 2100000000
+int Dynamic() {
+	int K, temp;
+	vector<int> file;
+	vector<int> sum;
+	vector<vector<int>> DP;
 
-vector<int> cost;
-vector<vector<int>> DP;
-
-int Dynamic(int start, int end) {
-	if (DP[start][end] != -1) return DP[start][end];
-	
-	//?
-	if (start == end) return 0;
-
-	//??
-	int sum = 0;
-	for (int i = start; i <= end; i++) {
-		sum += cost[i];
-	}
-	int ans = -1;
-}
-
-int merge() {
-
-	int chap;
-	int temp;
-
-	cin >> chap;
-	cost.clear();
-	for (int i = 0; i < chap; i++) {
+	file.push_back(0);
+	sum.push_back(0);
+	cin >> K;
+	for (int i = 0; i < K; i++) {
 		cin >> temp;
-		cost.push_back(temp);
+		file.push_back(temp);
+		sum.push_back(sum[i] + temp);
 	}
-	DP.resize(chap);
-	for (int i = 0; i < DP.size(); i++)
-		DP[i].resize(chap, -1);
+
+	DP.resize(K + 1);
+	for (int i = 0; i < K + 1; i++)
+		DP[i].resize(K + 1, 0);
+
+	/*
+	for (int i = 0; i < file.size(); i++) {
+		cout << file[i] << " ";
+	}
+	cout << endl;
+	*/
+	/*
+	for (int i = 0; i < sum.size(); i++) {
+		cout << sum[i] << " ";
+	}
+	cout << endl;
+	*/
 	
-	return 0;
+	//행, 길이
+	for (int i = 1; i <= K; i++) {
+		//열, 어디서부터 어디까지인가, i+j의 합은 항상 K까지, j는 시작점, i와 j의 합은 최소 2, j가 시작점
+		for (int j = 1; j <= K - i; j++) {
+			//위에서부터 min값을 찾기위해 임시로 max값처리
+			DP[j][i + j] = INF;
+			//좌우의 합을 가를 k
+			for (int k = j; k < i + j; k++) {
+				//기존 값과
+				//k로 나눈 좌우 DP값의 합과, j부터 i+j까지의 누적합을 더한것을
+				//비교해서 min값적용
+				DP[j][i + j] = min(DP[j][i + j], DP[j][k] + DP[k + 1][i + j] + sum[i + j] - sum[j - 1]);
+			}
+		}
+
+	}
+	
+	return DP[1][K];
 }
 
 int main(void) {
-
+	
 	vector<int> ans;
 	int T;
 
 	cin >> T;
-	while (T--) {
-		ans.push_back(merge());
-	}
+	while (T--)
+		ans.push_back(Dynamic());
 
-
-	for (int i = 0; i < ans.size(); i++) {
+	for (int i = 0; i < ans.size(); i++)
 		cout << ans[i] << endl;
-	}
 
 	return 0;
 }
@@ -77,31 +91,4 @@ int main(void) {
 40 30 30 50
 15
 1 21 3 4 5 35 5 4 3 5 98 21 14 17 32
-*/
-
-/*
-if (prefixSum[start][end] != 0) return prefixSum[start][end];
-
-	int ans = INF;
-	for (int i = start; i < end; i++) {
-
-		prefixSum[start][i] = DP(start, i);
-		prefixSum[i + 1][end] = DP(i + 1, end);
-
-		cout << start << "부터" << end << "까지의 최솟값은: ";
-		cout << prefixSum[start][i] + prefixSum[i + 1][end] << endl;
-
-		ans = min(ans, prefixSum[start][i] + prefixSum[i + 1][end]);
-	}
-	return ans;
-*/
-
-/*
-
-	for (int i = 0; i < prefixSum.size(); i++) {
-		for (int j = 0; j < prefixSum[i].size(); j++) {
-			cout << prefixSum[i][j] << "\t";
-		}
-		cout << endl;
-	}
 */
