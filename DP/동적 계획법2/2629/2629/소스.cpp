dc18dfddcd7f381add1437e9f78815a4b070eb7f
Calli-T <joy14479@gmail.com>
2021-08-31 13:33:11 +0900

    2629번은 재귀 DP다 DP[i][j]에서 i는 사용하는 추의 번호를 j는 무게를 의미하는데, 해당값이 true라면 어떤 방식을 사용했던 간에 해당 추까지의 추들을 사용해서 해당 무게를 만들 수 있었다는 얘기이다, 추는 하나씩 늘려나간다, 저울에 올릴지, 안올릴지, 반대로올릴지(감산) 모두 재귀로 해야한다

--- DP/동적 계획법2/2629/2629/소스.cpp
+++ DP/동적 계획법2/2629/2629/소스.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-
-int N, M; //Ãß¿Í ±¸½½ÀÇ °³¼ö
+int N, M;
 int temp;
 vector<int> weight;
 vector<int> marble;
 vector<bool> isMeasurable;
+vector<vector<bool>> measured;
 
 void solution(int now, int total) {
+	if (now > N) return;
+	if (measured[now][total]) return;
 
-	total = total < 0 ? -total : total;
 	isMeasurable[total] = true;
+	measured[now][total] = true;
 
-	if (now <= weight.size() - 1) {
-		solution(now + 1, total);
-		solution(now + 1, total + weight[now]);
-		solution(now + 1, total - weight[now]);
-	}
+	solution(now + 1, total);
+	solution(now + 1, total + weight[now]);
+	solution(now + 1, abs(total - weight[now]));
 }
 
 int main(void) {
-
-	weight.push_back(0);
 	cin >> N;
+	weight.resize(N + 1);
 	for (int i = 0; i < N; i++) {
 		cin >> temp;
-		weight.push_back(temp);
+		weight[i] = temp;
 	}
 	cin >> M;
 	for (int i = 0; i < M; i++) {
@@ -36,15 +36,15 @@ int main(void) {
 		marble.push_back(temp);
 	}
 
-	temp = 0;
-	for (int i = 1; i < weight.size(); i++)
-		temp += weight[i];
-	isMeasurable.resize(temp + 1, false);
+	measured.resize(N + 1);
+	for (int i = 0; i < N + 1; i++)
+		measured[i].resize(15001, false);
+	isMeasurable.resize(15001, false);
 
-	solution(1, 0);
-
+	solution(0, 0);
 
 	for (int i = 0; i < marble.size(); i++) {
+
 		if (marble[i] > 15000) {
 			cout << "N ";
 			continue;
@@ -52,6 +52,7 @@ int main(void) {
 
 		if (isMeasurable[marble[i]]) cout << "Y ";
 		else cout << "N ";
+
 	}
 
 	return 0;
