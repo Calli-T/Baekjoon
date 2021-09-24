4d0c138b7242aaab76df3e2dce2f06e39d5836f7
Calli-T <joy14479@gmail.com>
2021-09-24 09:57:23 +0900

    구글링해서 찾은 작동하는코드다

--- BFS와 DFS/1199/1199/소스.cpp
+++ BFS와 DFS/1199/1199/소스.cpp
@@ -1,82 +1,52 @@
-#include <string>
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#define MAX 1000
+#include <stdio.h>
+
 using namespace std;
 
-// 정점의 수
 int n;
+int nxt[1010];
+int adj[1010][1010];
+vector<int> route;
 
-//
-vector<vector<int>> graph;
-
-// 인접행렬
-int adj_matrix[MAX][MAX];
-
-// 정점 차수
-int degree[MAX];
-
-// 오일러 회로 탐색 함수
-void eulerain_circuit(int current_node) {
-
-    // 현재 정점의 차수 계산
-    for (int i = 0; i < n; ++i) {
-
-        // 간선이 존재한다면
-        if (adj_matrix[current_node][i] != 0) {
-
-            // 간선을 가지는 정점의 차수를 -1 씩 감소한 뒤, 연결된 정점을 탐색
-            adj_matrix[current_node][i] -= 1;
-            adj_matrix[i][current_node] -= 1;
-            eulerain_circuit(i);
+void getEuler(int here) {
+    //변수  앞에 &가 붙으면 해당변수의 별명을 선언하겠다는 뜻
+    for (int& there = nxt[here]; there <= n; there++) {
+        while (adj[here][there]) {
+            adj[here][there]--;
+            adj[there][here]--;
+            getEuler(there);
         }
     }
-    // 현재 정점 출력
-    cout << to_string(current_node + 1) << " ";
+    route.push_back(here);
 }
 
-int main(void) {
+int main() {
+    int startPoint = 0;
+    int edgeNum[1010] = { 0, };
 
-    // 정점 개수 입력
     cin >> n;
 
-    graph.resize(n);
-
-    // 입력에 따른, 인접행렬 초기화
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-
-            // 인접행렬 요소 입력
-            cin >> adj_matrix[i][j];
-            if (adj_matrix[i][j])
-                graph[i].push_back(j);
-
-            // 인접행렬의 한 행을 모두 탐색하며, 한 정점의 모든 간선 개수 덧셈
-            degree[i] += adj_matrix[i][j];
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> adj[i][j];
+            edgeNum[i] += adj[i][j];
         }
     }
 
-    // 인접행렬 한 정점의 차수가 홀수라면 오일러 회로 불가능
-    for (int i = 0; i < n; ++i) {
-        if (degree[i] % 2 == 1) {
-            cout << -1 << endl;
+    for (int i = 1; i <= n; i++) {
+        // 하나의 엣지라도 붙어있어야 시작점이 될 수 있다.
+        if (edgeNum[i] != 0) startPoint = i;
+        // 한 노드에 엣지가 홀수개라면 끝
+        if (edgeNum[i] % 2 == 1) {
+            cout << -1;
             return 0;
         }
     }
-    
-    cout << endl;
-    for (int i = 0; i < n; i++) {
-        //???
-        for (int j = 0; j <n; j++) {
-            cout << graph[i][j] + 1 << " ";
-        }
-        cout << endl;
+    for (int i = 1; i <= n; i++) nxt[i] = 1;
+    getEuler(startPoint);
+
+    for (int i = 0; i < route.size(); i++) {
+        cout << route[i] << ' ';
     }
-
-    // 재귀 함수 호출
-    eulerain_circuit(0);
-
-    return 0;
-
 }
