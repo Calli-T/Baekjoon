77df32fac9e99d63d034560b9e52b8974a4b9ce2
Calli-T <joy14479@gmail.com>
2021-12-16 18:01:14 +0900

    시뮬 19238번 잊지말고 풀것 능지 떡락한게 눈에 보임

--- 문자열/단계별3/알고리즘/Aho-corasick/Basic/Basic/소스.cpp
+++ 문자열/단계별3/알고리즘/Aho-corasick/Basic/Basic/소스.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <queue>
 
+using namespace std;
+
 struct Trie {
 	Trie* ch[26];
 	Trie* fail;
@@ -28,21 +30,105 @@ struct Trie {
 void makeFail(Trie* root) {
 	queue<Trie*> Q;
 	root->fail = root;
-	
+
 	Q.push(root);
 	while (!Q.empty()) {
-		auto cur = Q.front();
+		Trie* cur = Q.front();
 		Q.pop();
 
-		for(int i = 0;  i< 26;)
-	}
+		for (int i = 0; i < 26; i++) {
+			Trie* next = cur->ch[i];
+			if (!next) continue;
+			
+			if (cur == root) next->fail = root;
+			else {
+				Trie* dest = cur->fail;
+				//root가 아닐때, 그리고 dest의 문자가 존재하지 않을 때(계속틀리는경우) 작동
+				while (dest != root && !dest->ch[i])
+					dest = dest->fail;
+				if (dest->ch[i]) dest = dest->ch[i];
+				next->fail = dest;
+			}
+			//output, 혹은isEnd는
+			//output노드에 도달 했을 때 거기 적힌 문자열들이 원문에 등장한다는 것이 확정
+			//she의 실패링크로 he를 보냈고, he가 완전한 단어라면 반드시 s'he'의 he도 true인것
+			if (next->fail->isEnd) next->isEnd = true;
 
+			Q.push(next);
+		}
+	}
 }
 
+void search(Trie *root) {
+	Trie* cur = root;
+	char str[10001];
+	cin >> str;
+	bool result = false;
 
-using namespace std;
+	for (int i = 0; str[i]; i++) {
+		int next = str[i] - 'a';
+		
+		while (cur != root && !cur->ch[next])
+			cur = cur->fail;
+		if (cur->ch[next]) cur = cur->ch[next];
+
+		if (cur->isEnd) {
+			result = true;
+			break;
+		}
+	}
+
+	if (result) cout << "YES"; else cout << "NO"; cout << '\n';
+}
 
 int main(void) {
+	int N, M;
+	char str[10001];
+	Trie* root = new Trie;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		root->insert(str);
+	}
+
+	makeFail(root);
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		search(root);
+		/*cin >> str;
+		Trie* cur = root;
+		bool result = false;
+
+		for (int c = 0; str[c]; c++) {
+			int next = str[c] - 'a';
+			//일치할 때까지, 혹은 아무것도 없을 때까지 fail탄다
+			while (cur != root && !cur->ch[next])
+				cur = cur->fail;
+			//일치하면 성공링크 타고 내려간다
+			if (cur->ch[next]) cur = cur->ch[next];
+
+			if (cur->isEnd) {
+				result = true;
+				break;
+			}
+		}
+		if (result) cout << "YES";
+		else cout << "NO";
+		cout << '\n';*/
+	}
+
+	delete root;
 
 	return 0;
 }
+
+
+/*
+검색함수
+1. 현재 위치, 검사할 패턴위치를 제작
+2. 일치하거나 root갈 때까지 fail탄다
+3. 일치하면 성공링크 탄다
+4. output이면 break걸고 존재함
+*/
---
+++ 시뮬레이션/19238/19238/소스.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#define NOW [now.r][now.c]
+#define NEXT [next.r][next.c]
+
+struct loc {
+	int r;
+	int c;
+};
+
+int dr[] = { 0, 0 , 1, -1 };
+int dc[] = { 1,  -1, 0, 0 };
+
+int N, M, F; //필드 크기, 손님수, 연료
+vector<vector<int>> field;
+vector<vector<bool>> visited;
+vector<int> row;
+vector<vector<int>> ST; // start
+vector<vector<int>> ED; // end
+loc start;
+loc nextDest;
+
+void resetVisited() {
+	visited.clear();
+	//방문체크 세팅
+	visited.resize(N + 2);
+	for (int i = 0; i < N + 2; i++)
+		visited[i].resize(N + 2, false);
+}
+
+void setField() {
+	int r, c;
+
+	cin >> N >> M >> F;
+	row.resize(N + 2, 1);
+	field.resize(N + 2, row);
+	
+	resetVisited();
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> field[i][j]; // 0은 공간, 1은벽
+		}
+	}
+
+	//출발지 설정
+	cin >> start.r >> start.c;
+
+	for (int i = 0; i < M; i++) {
+		cin >> r >> c;
+		field[r][c] = 2; // 출발지
+		cin >> r >> c;
+		field[r][c] = 3; // 목적지
+	}
+}
+
+void printField() {
+	for (int i = 0; i < N + 2; i++) {
+		for (int j = 0; j < N +2; j++) {
+			cout << field[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+//거리 반환용
+int BFS(loc& now, int pur) {
+	queue<loc> Q;
+	int size = 1;
+
+	resetVisited();
+	Q.push(now);
+	visited NOW = true;
+	while (!Q.empty()) {
+		auto cur = Q.front();
+		Q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			loc next;
+			next.r = now.r + dr[i];
+			next.c = now.c + dc[i];
+
+			
+		}
+	}
+
+	return 0;
+}
+
+int simulate() {
+	loc now;
+	now.r = start.r;
+	now.c = start.c;
+
+	cout << BFS(now, 2);
+
+	return 0;
+}
+
+int main(void) {
+	
+
+	setField();
+	printField();
+	cout << simulate();
+
+	return 0;
+}
